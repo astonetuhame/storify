a0c2f454974663da6063143a6c557bca633c5503
Astone Tuhame <astonetuhame@gmail.com>
2021-12-16 18:25:59 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group( function() {
+    Route::resource('stories', StoriesController::class);
+    // ->name('stories.index');
+    // Route::get('/stories/{story}', 'StoriesController@show')->name('stories.show');
 });
