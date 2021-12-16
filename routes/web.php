99d2cc4bc4afcf938a346cf54036dac4eecec07c
Astone Tuhame <astonetuhame@gmail.com>
2021-12-16 18:32:58 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,7 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Auth::routes();
-
-// Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/', [LoginController::class, 'showLoginForm']);
 
