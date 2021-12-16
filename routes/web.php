bb3baf68d6b04384471739debbd352823f4293dc
Astone Tuhame <astonetuhame@gmail.com>
2021-12-16 18:33:27 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StoriesController;
 use App\Http\Controllers\Auth\LoginController;
 
