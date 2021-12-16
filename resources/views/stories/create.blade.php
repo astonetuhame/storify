2376ef693d09ae62d4833abc60371a4c1308f981
Astone Tuhame <astonetuhame@gmail.com>
2021-12-16 18:25:40 +0300

    Create create.blade.php

---
+++ resources/views/stories/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    Add Story
+
+                    <a href=" {{ route('stories.index') }}" class="float-right">Back</a>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{ route('stories.store') }}" method="POST">
+                        @csrf
+
+                        @include('stories.form')
+                        
+
+                        <button class="btn btn-primary">Add</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
