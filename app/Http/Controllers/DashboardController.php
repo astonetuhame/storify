4bd17091b488065dc8077478d61c0405fc255db5
Astone Tuhame <astonetuhame@gmail.com>
2021-12-21 17:51:28 +0300

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,18 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Story;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         //
-        $stories = Story::where('status', 1)
+        // DB::enableQueryLog();
+        $query = Story::where('status', 1);
+        $type = request()->input('type');
+        if (in_array($type, ['short', 'long'])) {
+            $query->where('type', $type);
+        }
+        $stories = $query->with('user')
             ->orderBy('id', 'DESC')
             ->paginate(10);
 
         return view('dashboard.index', [
             'stories' => $stories
+        ]);
+    }
+
+    public function show(Story $activeStory)
+    {
+        //
+        return view('dashboard.show', [
+            'story' => $activeStory
         ]);
     }
 }
