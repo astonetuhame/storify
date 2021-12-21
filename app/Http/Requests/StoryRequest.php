f9e7049dd6f46e852a943fd7f1126b8db535b958
Astone Tuhame <astonetuhame@gmail.com>
2021-12-21 13:40:08 +0300

    Update validation rules

--- app/Http/Requests/StoryRequest.php
+++ app/Http/Requests/StoryRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoryRequest extends FormRequest
@@ -23,11 +24,38 @@ class StoryRequest extends FormRequest
      */
     public function rules()
     {
+        $storyId = $this->route('story.id');
         return [
-            'title' => 'required',
-            'body' => 'required',
-            'type' => 'required',
-            'status' => 'required',
+            'title' => [
+                'required', 'min:10', 'max:50',
+                function ($attribute, $value, $fail) {
+                    if ($value == 'Dummy Title') {
+                        $fail($attribute . ' is not valid');
+                    }
+                },
+                //ignore title has already been taken validation when editing
+                Rule::unique('stories')->ignore($storyId)
+            ],
+            'body' => ['required', 'min:50'],
+            'type' => ['required'],
+            'status' => ['required'],
         ];
+    }
+
+    // custom messages for validation errors
+    public function messages()
+    {
+        // 'title.required' => 'Please enter title'
+        return [
+            'required' => 'Please enter :attribute'
+        ];
+    }
+
+    public function withValidator($v)
+    {
+        // body should have a max of 200 characters when type is short
+        $v->sometimes('body', 'max:200', function ($input) {
+            return 'short' == $input->type;
+        });
     }
 }
