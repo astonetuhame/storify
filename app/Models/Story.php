6ac68ef0322ff01c4091ed436ec585694ea61bb5
Astone Tuhame <astonetuhame@gmail.com>
2021-12-21 17:50:58 +0300

    Update Story.php

--- app/Models/Story.php
+++ app/Models/Story.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Story extends Model
@@ -24,5 +25,12 @@ class Story extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected static function booted()
+    {
+        // static::addGlobalScope('active', function (Builder $builder) {
+        //     $builder->where('status', 1);
+        // });
     }
 }
