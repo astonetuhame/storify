5ec4e81cbd50f18ba6057a578ab7cd592e608d18
Astone Tuhame <astonetuhame@gmail.com>
2021-12-29 16:56:47 +0300

    Update show.blade.php

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -6,14 +6,29 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    {{ $story->title }} by {{ $story->user->name }}
+                    {{ $story->title }} by {{ $story->user->name}}
 
-                    <a href=" {{ route('dashboard.index') }}" class="d-flex justify-content-end">Back</a>
+                    <a href=" {{ route('dashboard.index') }}" class="float-right">Back</a>
                 </div>
 
                 <div class="card-body">
-                    {{ $story->body}}
-                    <p class="font-italic">{{ $story->footnote }}</p>
+                    <img class="card-img-top" src="{{ $story->thumbnail}}" alt="Card image cap">
+                    <!-- {{ $story->body}}
+
+                    <p class="font-italic">{{ $story->footnote}}</p> -->
+                    <p class="card-text">{{ $story->body }}</p>
+                    @foreach ( $story->tags as $tag )
+                    <button class="btn btn-sm btn-outline-primary">
+                        {{ $tag->name }}
+                    </button>
+                    @endforeach
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button"
+                                class="btn btn-sm btn-outline-secondary">{{$story->user->name}}</button>
+                        </div>
+                        <small class="text-muted">{{ $story->type}}</small>
+                    </div>
                 </div>
             </div>
         </div>
