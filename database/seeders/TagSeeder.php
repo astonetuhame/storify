70b3762b2498b5ded7d0f75d3f14ae3bd6cbeb04
Astone Tuhame <astonetuhame@gmail.com>
2021-12-29 16:56:02 +0300

    Create TagSeeder.php

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['PHP', 'Laravel', 'Bootstrap', 'CSS', 'Html', 'Javascript', 'Vue'];
+
+        foreach ($tags as $tag) {
+            DB::table('tags')->insert(['name' => $tag]);
+        }
+    }
+}
