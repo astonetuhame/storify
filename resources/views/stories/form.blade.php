941c3d14bb4e960031c52016401ee0b1b8576273
Astone Tuhame <astonetuhame@gmail.com>
2021-12-29 16:56:43 +0300

    Update form.blade.php

--- resources/views/stories/form.blade.php
+++ resources/views/stories/form.blade.php
@@ -67,3 +67,14 @@
     @enderror
     <img src="{{ $story->thumbnail }}" />
 </div>
+
+<div class="form-group">
+    @foreach ($tags as $tag)
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" name="tag[]" value="{{ $tag->id }}" {{ in_array($tag->id,
+        old('tags', $story->tags->pluck('id')->toArray())) ? 'checked' : "" }}
+        >
+        <label class="form-check-label">{{ $tag->name }}</label>
+    </div>
+    @endforeach
+</div>
